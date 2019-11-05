40215dcbd40880f3a5d9087ecc37ed688abb72d3
Ilya Petrov <dnamix1@gmail.com>
2019-11-05 14:14:32 +0300

    added fetching movies with Promise functionality and passing it into individual movie details page by ID

--- actions/index.js
+++ actions/index.js
@@ -63,3 +63,11 @@ export const getMovies = () => {
     }, 400);
   });
 };
+
+export const getMovieById = id => {
+  return new Promise((resolve, reject) => {
+    const movieIndex = MOVIE_DATA.findIndex(m => m.id === id);
+    const movie = MOVIE_DATA[movieIndex];
+    setTimeout(() => resolve(movie), 50);
+  });
+};
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,12 +1,36 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { getMovieById } from "../../actions";
 
-export default function Movie() {
+const Movie = props => {
   const router = useRouter();
   const { id } = router.query;
+  const { movie } = props;
   return (
     <div className="container">
-      <h1>Movie with id: {id}</h1>
+      <div className="jumbotron">
+        <h1 className="display-4">{movie.name}</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr className="my-4" />
+        <p>
+          It uses utility classNamees for typography and spacing to space
+          content out within the larger container.
+        </p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </div>
+      <p>Some movie description for the movie</p>
     </div>
   );
-}
+};
+
+Movie.getInitialProps = async () => {
+  const movie = await getMovieById(id);
+  return { movie };
+};
+
+export default Movie;
